6ce97c4d3d0dae363a3eb3c362862a093f60e1f9
Lukasz Spas <lukasz.spas@live.com>
2021-11-19 00:32:21 +0100

    First version

---
+++ android/build/generated/source/buildConfig/debug/com/oila/minimizer/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.oila.minimizer;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String LIBRARY_PACKAGE_NAME = "com.oila.minimizer";
+  public static final String BUILD_TYPE = "debug";
+}
